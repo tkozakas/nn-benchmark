845deb2e3c4e62049f87e89c2a5787ad3db7df35
tkozakas <tomas.kozakas@gmail.com>
2025-11-02 12:09:32 +0200

    update

--- run_train.sh
+++ run_train.sh
@@ -19,7 +19,7 @@ uv venv --python 3.12
 source .venv/bin/activate
 
 uv pip install -r requirements.txt
-uv pip install torch torchvision torchaudio
+uv pip install torch torchvision torchaudio nvidia-ml-py
 
 # run training
 cd src
