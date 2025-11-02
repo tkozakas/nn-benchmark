deaeb458f82f7d15f2ad6e0ff028f7a8a75d8123
tkozakas <tomas.kozakas@gmail.com>
2025-11-02 12:39:49 +0200

    update

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 mkdir dataset
 cd dataset
 
-curl -L -o . \
-https://www.kaggle.com/api/v1/datasets/download/akash2sharma/tiny-imagenet
+curl -L -o tiny-imagenet.zip https://www.kaggle.com/api/v1/datasets/download/akash2sharma/tiny-imagenet
 unzip tiny-imagenet.zip
 
 cd ..
